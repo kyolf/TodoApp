525310db001b31f282691ac897137f460b159e53
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-19 18:35:44 -0800

    initializing search list module

---
+++ components/search-bar/search-bar-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular
+  .module('search.bar')
+  .controller('SearchBarController', SearchBarController);
+
+function SearchBarController() {
+  var sBarVm = this;
+  sBarVm.list = [];
+  
+}
