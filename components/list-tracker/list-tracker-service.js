5a90334d9c5581a80e5bdc3340bd55109e7dcb90
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-20 19:13:44 -0800

    implemented the list tracker service

--- components/list-tracker/list-tracker-service.js
+++ components/list-tracker/list-tracker-service.js
@@ -2,4 +2,18 @@
 
 angular
   .module('list.tracker')
-  .service();
+  .service('listTracker', listTracker);
+
+function listTracker() {
+  this.todos = [];
+  this.getToDos = getToDos;
+  this.addToDo = addToDo;
+
+  function getToDos() {
+    return this.toDos;
+  }
+
+  function addToDo(el) {
+    this.toDos.push(el);
+  }
+}
