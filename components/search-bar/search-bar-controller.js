f41cd38e30ebf940ae52d8541a9e8d49f3e63990
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-21 16:59:46 -0800

    implemented add to list feature

--- components/search-bar/search-bar-controller.js
+++ components/search-bar/search-bar-controller.js
@@ -4,6 +4,11 @@ angular
   .module('search.bar')
   .controller('SearchBarController', SearchBarController);
 
-function SearchBarController() {
+function SearchBarController(listTracker) {
   var sBarVm = this;
+  sBarVm.addToList = addToList;
+
+  function addToList() {
+    listTracker.addToDo(sBarVm.toDoDesc);
+  }
 }
