0ac2349f1d209c516e14f4a50aa36907f7f5ae41
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-28 18:42:32 -0800

    palying ith watch

--- components/search-bar/search-bar-controller.js
+++ components/search-bar/search-bar-controller.js
@@ -4,10 +4,15 @@ angular
   .module('search.bar')
   .controller('SearchBarController', SearchBarController);
 
-function SearchBarController(listTracker) {
+function SearchBarController(listTracker, $scope) {
   var sBarVm = this;
   sBarVm.addToList = addToList;
+  sBarVm.toDoDesc = 'Enter Desc';
 
+  $scope.$watch('sBarVm.toDoDesc', function(newValue, oldValue) {
+    console.log('ll' + newValue);
+    console.log('hi' + oldValue);
+  });
   function addToList() {
     listTracker.addToDo(sBarVm.toDoDesc);
   }
